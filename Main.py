71d8ffeae6a1c06c722cabbf7c26e73a70f5bb43
Mawethu Madanile <mamadjhb025@student.wethinkcode.co.za>
2026-07-02 13:48:05 +0200

    made config file

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-New repo
+import os
+
+AWS_REGION = os.environ.get("CLOUD_REGION", )
+TABLE_NAME = os.environ.get("CLOUD_TABLE", )
+SNS_TOPIC_ARN = os.environ.get("CLOUDTASK_SNS_TOPIC_ARN")
+DUE_SOON_HOURS = int(os.environ.get("CLOUDTASK_DUE_SOON_HOURS", "24"))
+DEFUALT_USER_ID = os.environ.get("CLOUDTASK_USER_ID", "defualt-user")
